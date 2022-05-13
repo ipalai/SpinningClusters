b451853772cfae6e9ba4c6627a57b7018f37add6
ipalai <ivan@MacBook-Pro-di-Ivan.local>
2022-05-13 16:11:51 +0200

    Spinning now works

--- Test_0/make_initconfig.py
+++ Test_0/make_initconfig.py
@@ -31,10 +31,8 @@ class make_particles(object):
         self.numMol = 0
 
         self.numBonds = 0
-        self.bondId = 0
 
         self.numAngles = 0
-        self.angleId = 0
 
         self.numTypes = 0
         self.type_mass_list =[]
@@ -43,26 +41,20 @@ class make_particles(object):
         def init_lattice_2d(lat_con):
             num_x = int(self.side)
             num_y = int(self.side)
-
             lattice_out = np.zeros(shape=(num_x*num_y, 3))
             counter = 0
             for i in range(num_x):
                 for j in range(num_y):
-
                     lattice_out[counter, 0] = lat_con * i - lat_con*num_x * 0.5
                     lattice_out[counter, 1] = lat_con * j - lat_con*num_y * 0.5
                     lattice_out[counter, 2] = 0.0
                     counter += 1
-
             return lattice_out
 
 
-
         self.lattice_sites = init_lattice_2d(self.lattice_constant)
-
         assert self.lattice_factor>1.1, "ERROR: lattice_factor<1.1, too many particles."
-
-        assert self.num_tot <= self.lattice_sites.shape[0], "ERROR: not enough lattice sites, increase box size."
+        assert self.numAll <= self.lattice_sites.shape[0], "ERROR: not enough lattice sites, increase box size."
 
         lattice_inds = np.arange(self.lattice_sites.shape[0])
 
@@ -72,13 +64,13 @@ class make_particles(object):
 
     def make_A(self):
 
-        particles_list = range(1,self.q_A+2)
+        particles_list = [1,2]+[3 for i in range(3,8+1)] # range(1,self.q_A+2)
         particle_types = len(particles_list)
         self.numTypes += len(np.unique(particles_list))
 
         MassAlpha = 0.1 * (self.sigma/self.PatchRadialDistance)**2  # fraction of mass due to patches such that moment of inertia equals that of a sphere
         MassCentralAtom = 1-MassAlpha
-        MassPatch = MassAlpha/self.q_a
+        MassPatch = MassAlpha/self.q_A
         for i in range(particle_types):
             if i+1==1:
                 to_add = [particles_list[i], MassCentralAtom]
@@ -95,15 +87,20 @@ class make_particles(object):
             y = self.lattice_sites[indBuf, 1]
             z = self.lattice_sites[indBuf, 2]
 
+            RandomPhase = np.random.rand() * 2 * np.pi
             for n in range(self.q_A+1):
                 self.numAll += 1
                 if (n == 0):
                     self.coords.append(
                         "\t " + str(self.numAll) + " " + str(self.numMol) + " "+str(particles_list[n])+" 0 " + str(x) + " " + str(y) + " " + str(
                             z) + " 0 0 0 \n")
-                randomPhase=np.random.rand()*2*np.pi
                 position_on_circle = self.PatchRadialDistance*np.array([[ np.cos(RandomPhase + i*2*np.pi/self.q_A), np.sin(RandomPhase + i*2*np.pi/self.q_A) ] for i in range(0,self.q_A)])
                 if n > 0:
                     self.coords.append(
                         "\t " + str(self.numAll) + " " + str(self.numMol) + " " +  str(particles_list[n]) + " 0 " + str(x - position_on_circle[n-1,0]) + " " + str(y - position_on_circle[n-1,1]) + " " + str(
                             z) + " 0 0 0 \n")
+                    # if n==1:
+                    #     self.numBonds += 1
+                    #     self.bonds.append(
+                    #         "\t {:d} {:d} {:d} {:d} \n".format(self.numBonds, 1, self.numAll-1, self.numAll)
+                    #     )
--- Test_0/make_simulation.py
+++ Test_0/make_simulation.py
@@ -7,12 +7,13 @@ import os
 import numpy as np
 
 
-def write_in_script(q_A, sigma, PatchRadius, PatchStrength, IsotropicAttrRange, IsotropicAttrStrength, real, RunSteps, dumpevery, filePattern, configName, ResultsFolder):
+def write_in_script(q_A, sigma, PatchRadius, PatchStrength, IsotropicAttrRange, IsotropicAttrStrength, real, RunSteps, dumpevery, filePattern, configName, ResultsFolder, extForce, box):
 
     seed = 100 + real
     filename = "Input/Scripts/Input_{:s}_{:d}.in".format(filePattern, real)
     Temperature = 1.0
     LangevinDamping = 0.1
+    Lx, Ly = box
 
 
     f = open(filename, "w")
@@ -25,8 +26,8 @@ def write_in_script(q_A, sigma, PatchRadius, PatchStrength, IsotropicAttrRange, 
     f.write("boundary               p p p \n")
     f.write("read_data              {:s} \n\n".format(configName))
 
-    f.write("group                  Central type 1")
-    f.write("group                  Patch type 2")
+    f.write("group                  Central type 1\n")
+    f.write("group                  Patch type 2\n\n")
 
     f.write("neighbor               0.5 bin\n")
     f.write("neigh_modify           every 1 delay 1\n")
@@ -34,40 +35,78 @@ def write_in_script(q_A, sigma, PatchRadius, PatchStrength, IsotropicAttrRange, 
 
     GlobalCutoff = sigma + IsotropicAttrRange
     f.write("pair_style             cosine/squared {:.4f} \n".format(GlobalCutoff))
-    f.write("pair_coeff             * * cosine/squared 0 1.0 1.0 wca \n")
+    f.write("pair_coeff             * * 0.0 1.0 1.0 wca \n")
 
     f.write("pair_coeff             1 1  {:.1f} {:.3f} {:.3f} wca \n".format(IsotropicAttrStrength, sigma, sigma + IsotropicAttrRange))
 
-    for id1 in range(2,q_A+2):
-        for id2 in range(id1,q_A+2):
+    numParticleTypes = 3 #q_A+2
+    for id1 in range(2,numParticleTypes):
+        for id2 in range(id1,numParticleTypes):
             f.write("pair_coeff             {:d} {:d}  {:.1f} {:.3f} {:.3f} \n".format(id1, id2, PatchStrength, 0.0, PatchRadius))  # mirrored cosine squared if PatchStrength<0 (purely repulsive)
 
 
 
-    f.write("\nvelocity               all create {:.3f} {:d} \n".format(Temperature,seed))
+    #f.write("\nvelocity               all create {:.3f} {:d} \n".format(Temperature,seed))
+    f.write("\nvelocity               all create {:.3f} {:d} \n".format(0,seed))
+
+
+    f.write("compute                cMol all chunk/atom molecule \n")
+    # extract positions of central atom and 1sdt patch for each molecule
+    f.write("compute                cCMCentral Central com/chunk cMol \n")
+    f.write("compute                cCMPatch Patch com/chunk cMol \n")
+
+    #f.write("compute                cCMatom all chunk/spread/atom cmol c_cCMmol \n")
+    # create two atom variables that return the position of the central atom of the molecule and the position of the 1st patch of the molecule
+    f.write("compute                cCentral all chunk/spread/atom cMol c_cCMCentral[*] \n")
+    f.write("compute                cPatch all chunk/spread/atom cMol c_cCMPatch[*] \n")
+
+    #f.write("compute                cCMatomx all chunk/spread/atom cmol c_cCMmol[1] \n")
+    #f.write("compute                cCMatomy all chunk/spread/atom cmol c_cCMmol[2] \n")
+    #f.write("variable               vCMx atom c_cCMatomx \n")
+    #f.write("variable               vCMy atom c_cCMatomy \n")
+    #f.write("variable               vCMax atom c_cCMmol[*][1] \n")
+    #f.write("variable               vCMay atom c_cCMmol[*][2] \n")
+    # Move Central and Patch stored positions such that if patch and central are across boundaries, computed force is still correct
+    # Remember that vCentralx and vCentraly are only used for patch atoms and viceversa.
+    f.write("variable               vCentralx atom \"(abs(c_cCentral[1]-x)<{:f}) * c_cCentral[1] + (abs(c_cCentral[1]-x)>={:f}) * (c_cCentral[1]+(x>0)*{:f})\"  \n".format(0.5*Lx, 0.5*Lx, Lx))
+    f.write("variable               vCentraly atom \"(abs(c_cCentral[2]-y)<{:f}) * c_cCentral[2] + (abs(c_cCentral[2]-y)>={:f}) * (c_cCentral[2]+(y>0)*{:f})\"  \n".format(0.5*Ly, 0.5*Ly, Ly))
+    f.write("variable               vPatchx atom \"(abs(c_cPatch[1]-x)<{:f}) * c_cPatch[1] + (abs(c_cPatch[1]-x)>={:f}) * (c_cPatch[1]+(x>0)*{:f})\"  \n".format(0.5*Lx, 0.5*Lx, Lx))
+    f.write("variable               vPatchy atom \"(abs(c_cPatch[2]-y)<{:f}) * c_cPatch[2] + (abs(c_cPatch[2]-y)>={:f}) * (c_cPatch[2]+(y>0)*{:f})\"  \n".format(0.5*Ly, 0.5*Ly, Ly))
+
+    # compute forces
+    f.write("# extForce = {:f} \n".format(extForce))
+    # here x and y are positions of the patch:q
+
+    f.write("variable               fxPatch   atom  {:f}*(y-v_vCentraly) \n".format(extForce / PatchRadialDistance))
+    f.write("variable               fyPatch   atom -{:f}*(x-v_vCentralx) \n".format(extForce / PatchRadialDistance))
+    #f.write("variable               fxPatch   atom  {:f}*(y-c_cCMatomy) \n".format(extForce/PatchRadialDistance))
+    #f.write("variable               fyPatch   atom -{:f}*(x-c_cCMatomx) \n".format(extForce/PatchRadialDistance))
+    # now x and y are positions of the central atom
+    f.write("variable               fxCentral atom -{:f}*(v_vPatchy-y) \n".format(extForce/PatchRadialDistance))
+    f.write("variable               fyCentral atom  {:f}*(v_vPatchx-x) \n\n".format(extForce/PatchRadialDistance))
+    #f.write("variable               fxCentral atom -{:f}*(y-v_vCMy) \n".format(extForce/PatchRadialDistance))
+    #f.write("variable               fyCentral atom  {:f}*(x-v_vCMx) \n\n".format(extForce/PatchRadialDistance))
 
     f.write("thermo                 {:d}\n".format(dumpevery))
-    f.write("thermo_style           custom step temp press etotal epair\n")
+    f.write("thermo_style           custom step temp press etotal epair \n")
     f.write("thermo_modify          flush yes\n")
     f.write("timestep               0.01\n\n")
 
-    f.write("compute                cmol all chunk/atom molecule \n")
-    f.write("compute                cCMmol all com/chunk cmol \n")
-    f.write("variable               vCMx atom c_cCMmol[mol][1] \n")
-    f.write("variable               vCMy atom c_cCMmol[mol][2] \n")
-    f.write("variable               fxPatch   atom  {:f}*(y-v_vCMy)/{:f} \n".format(extForce,PatchRadialDistance))
-    f.write("variable               fyPatch   atom -{:f}*(x-v_vCMx)/{:f} \n".format(extForce,PatchRadialDistance))
-    f.write("variable               fxCentral atom -{:f}*(y-v_vCMy)/{:f} \n".format(extForce,PatchRadialDistance))
-    f.write("variable               fyCentral atom  {:f}*(x-v_vCMx)/{:f} \n\n".format(extForce,PatchRadialDistance))
-
-    f.write("fix                    fLANG all langevin {:.3f} {:.3f} {:.3f} {:d} \n".format(Temperature, Temperature, LangevinDamping, seed))
+    #f.write("fix                    fLANG all langevin {:.3f} {:.3f} {:.3f} {:d} \n".format(Temperature, Temperature, LangevinDamping, seed))
     # f.write("fix                    fNVE all nve\n")
     f.write("fix                    fRigidNVE all rigid/nve molecule\n")
     f.write("fix                    fEnforce2d all enforce2d\n")
-    f.write("fix                    fTorqueCentral Central v_fxCentral v_fyCentral 0 \n")
-    f.write("fix                    fTorquePatch Patch v_fxPatch v_fyPatch 0 \n\n")
 
+    #f.write("fix                    prova all momentum 1 linear 1 1 0\n")
     f.write("dump                   1 all custom {:d} {:s}/Traj_{:s}_{:d}.xyz id type mol x y z \n\n".format(dumpevery, ResultsFolder, filePattern, real))
+    f.write("dump                   101 all custom {:d} {:s}/cCMatom_{:s}_{:d}.dat id type mol v_vCentralx v_vCentraly v_vPatchx v_vPatchy v_fxPatch v_fyPatch v_fxCentral v_fyCentral  \n".format(dumpevery,ResultsFolder,filePattern,real))
+    f.write("dump_modify            1 sort id \n")
+    f.write("dump_modify            101 sort id \n")
+
+    f.write("run                    {:d}\n".format(1000))
+
+    f.write("fix                    fTorqueCentral Central addforce v_fxCentral v_fyCentral 0 \n")
+    f.write("fix                    fTorquePatch Patch addforce v_fxPatch v_fyPatch 0 \n\n")
 
     f.write("run                    {:d}\n".format(RunSteps))
 
@@ -83,20 +122,18 @@ def write_in_script(q_A, sigma, PatchRadius, PatchStrength, IsotropicAttrRange, 
 
 if __name__ == "__main__":
 
-    dumpevery = 5e5
-    EqSteps = 1e5
-    RunSteps = 5e7
+    dumpevery = 10
+    RunSteps = 10000
 
     ## Simulation parameters
 
     Energy_Patches = 10.0
     Energy_A = 10.0
     
-    num_A = 200
-    num_tot = num_A
-    density_A = 0.25 # number of colloids per sigma^-2. A density of 0.25 corresponds to a packing fraction of 0.2
+    num_A = 2
+    density_A = 0.020 # number of colloids per sigma^-2. A density of 0.25 corresponds to a packing fraction of 0.2
 
-    qA = 6
+    q_A = 6
 
     sigma = 1.0
     IsotropicAttrRange = 0.2
@@ -108,6 +145,8 @@ if __name__ == "__main__":
 
     Temperature = 1
 
+    extTorque = 0.1
+
     real = 0
 
 
@@ -116,17 +155,19 @@ if __name__ == "__main__":
     #parser.add_argument('--eps_ll', '-eps_ll', dest='eps_ll', action='store', type=float, default=eps_ll, help='Energy depth of attraction/repulsion between cross patchs')
     parser.add_argument('--numA', '-nA', dest='num_A', action='store', type=int, default=num_A, help='Number of aggregating colloids.')
     parser.add_argument('--density_A', '-density_A', '-dens', dest='density_A', action='store', type=float, default=density_A, help='Surface density of colloids in units of sigma^-2 (multiply by 0.25*pi*sigma^2 to obtain packing fraction).')
-    parser.add_argument('--q_A', '-qA', dest='qA', action='store', type=int, default=q_A, help='Number of patches on 1 colloid.')
+    parser.add_argument('--q_A', '-qA', dest='q_A', action='store', type=int, default=q_A, help='Number of patches on 1 colloid.')
     #parser.add_argument('--patch_sigma', '-ps', dest='patch_sigma', action='store', type=float, default=patch_sigma, help='Diameter of patch')
     parser.add_argument('--PatchRadius', '-pr', '-rp', dest='PatchRadius', action='store', type=float, default=PatchRadius, help='Radius of patches.')
-    parser.add_argument('--PatchRadialDistance', '-pd', '-dp' dest='PatchRadialDistance', action='store', type=float, default=PatchRadialDistance, help='Radial distance of patches from centre of colloid.')
+    parser.add_argument('--PatchRadialDistance', '-pd', '-dp', dest='PatchRadialDistance', action='store', type=float, default=PatchRadialDistance, help='Radial distance of patches from centre of colloid.')
     parser.add_argument('--PatchStrength', '-ps', '-ep', dest='PatchStrength', action='store', type=float, default=PatchStrength, help='Radial distance of patches from centre of colloid.')
     parser.add_argument('--IsotropicAttrRange', '-ra', dest='IsotropicAttrRange', action='store', type=float, default=IsotropicAttrRange, help='Attraction range of the cosine-squared isotropic attraction between molecules.')
     parser.add_argument('--IsotropicAttrStrength', '-ea', dest='IsotropicAttrStrength', action='store', type=float, default=IsotropicAttrStrength, help='Strength of the cosine-squared isotropic attraction between molecules.')
-    parser.add_argument('--real','-real', dest='real', action='store', type=int, default=0, help='Number of realisation (statistics), sets the seed.')
-    parser.add_argument('--Temperature','-T', dest='Temperature', action='store', type=float, default=0, help='Temperature.')
+    parser.add_argument('--real','-real', dest='real', action='store', type=int, default=real, help='Number of realisation (statistics), sets the seed.')
+    parser.add_argument('--Temperature','-T', dest='Temperature', action='store', type=float, default=Temperature, help='Temperature.')
     parser.add_argument('-runsteps','--RunSteps', dest='RunSteps', action='store', type=int, default=RunSteps, help='Number of time steps.')
     parser.add_argument('-dumpevery','--dumpevery', dest='dumpevery', action='store', type=int, default=dumpevery, help='Number of skipped time steps in dump file.')
+    parser.add_argument('-extTorque','--extTorque', '-eT', dest='extTorque', action='store', type=int, default=extTorque, help='Number of skipped time steps in dump file.')
+
 
     args = parser.parse_args()
 
@@ -141,13 +182,14 @@ if __name__ == "__main__":
     real = args.real
     RunSteps = args.RunSteps
     dumpevery = args.dumpevery
+    extTorque = args.extTorque
 
     GlobalCutoff = sigma + IsotropicAttrRange
-
+    extForce = extTorque/PatchRadialDistance
 
     ## Initial configuration
 
-    side = np.ceil(np.sqrt(num_tot))
+    side = np.ceil(np.sqrt(num_A))
     lattice_factor = np.sqrt(1.0*num_A/(side*side)/density_A)       ## Ensures that A surface density is exactly 0.0075 (protein_sigma)^-2
     densitycheck = num_A/(side*lattice_factor)**2                         ## In units of (protein_sigma)^-2
     assert np.isclose(density_A,densitycheck), "ERROR: Densities do not correspond."
@@ -155,14 +197,13 @@ if __name__ == "__main__":
     system = make_particles(sigma, PatchRadius, PatchRadialDistance, num_A, q_A, side, lattice_factor, real)
     system.make_A()
 
-
-    filePattern = "qA{:d}_dp{:.2f}_dens{:.3f}_nA{:d}_rp{:.2f}_ra{:.2f}_ep{:.1f}_ea{:.1f}_T{:.1f}".format(q_A,PatchRadialDistance,density_A,num_A,PatchRadius,IsotropicAttrRange,PatchStrength,IsotropicAttrStrength,Temperature)
+    filePattern = "qA{:d}_dp{:.2f}_dens{:.3f}_nA{:d}_rp{:.2f}_ra{:.2f}_ep{:.1f}_ea{:.1f}_eT{:.5f}_T{:.1f}".format(q_A,PatchRadialDistance,density_A,num_A,PatchRadius,IsotropicAttrRange,PatchStrength,IsotropicAttrStrength,extTorque,Temperature)
     ResultsFolder = "Results_qA{:d}_dp{:.2f}_dens{:.3f}".format(q_A,PatchRadialDistance,density_A)
 
     if not os.path.exists(ResultsFolder):
         os.makedirs(ResultsFolder)
 
-    configName = "Config_{:s}_{:d}.dat".format(filePattern,real)
+    configName = "Input/Configurations/Config_{:s}_{:d}.dat".format(filePattern,real)
 
     header = ["LAMMPS Description \n \n",
               "\t " + str(system.numAll) + " atoms \n \t " + str(system.numBonds) +
@@ -170,22 +211,20 @@ if __name__ == "__main__":
               "\n \t "+str(system.numTypes)+" atom types \n \t 2 bond types \n \t 0 angle types \n \t 0 dihedral types \n \t 0 improper types \n",
               "\n \t " + str(-system.Lx*0.5) + " " + str(system.Lx*0.5) + " xlo xhi\n \t", str(-system.Ly*0.5) + " " + str(system.Ly*0.5) + " ylo yhi \n \t",
               str(-system.Lz*0.5) + " " + str(system.Lz*0.5) + " zlo zhi\n"]
-
     header.append("\nMasses \n \n")
     for i in range(len(system.type_mass_list)):
         header.append("\t {:d} {:.5f} \n".format(system.type_mass_list[i][0],system.type_mass_list[i][1]))
 
     f = open(configName, "w")
-
     for item in header:
         f.write("{:s} ".format(item))
-
     for item in system.coords:
         f.write("{:s} ".format(item))
-
-
+    # for itm in system.bonds:
+    #     f.write("{:s} ".format(item))
     f.close()
 
     ## Write input script
-    inputscriptfile = write_in_script(q_A, sigma, PatchRadius, PatchStrength, IsotropicAttrRange, IsotropicAttrStrength, real, RunSteps, dumpevery, filePattern, configName, ResultsFolder)
-    clusterscriptfile = write_cluster_script(MPInum)
+    inputscriptfile = write_in_script(q_A, sigma, PatchRadius, PatchStrength, IsotropicAttrRange, IsotropicAttrStrength, real, RunSteps, dumpevery, filePattern, configName, ResultsFolder, extForce, [system.Lx, system.Ly])
+    print(inputscriptfile)
+    #clusterscriptfile = write_cluster_script(MPInum)
