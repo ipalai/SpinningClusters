778c501d87a2f6948beaec1bce1b40e2662ff04a
ipalaia <40635629+ipalai@users.noreply.github.com>
2022-05-16 19:25:48 +0200

    changed runcluster

--- Test_1/runcluster.sh
+++ Test_1/runcluster.sh
@@ -4,21 +4,17 @@ module load python3/recommended
 nA=200
 mpinum=1
 
-for qA in 5
-do
-for qB in ${qA}
-do
-for EPS in 5 #5 6 8 10 #6 7 8 #7 8 9 10 1
-do
-for nB in 50 100 150 200 300 400 500 800 1200 1600  #50 100 150 200 300 500 800
-do
 for REAL in 311 312 313 314 315 #301 302 303 304 305 #1 2 3 #51 52 
 do
-for EPSa in 1.5 #0.5 1.0 1.5 2.0 2.5 3.0 #1 1.5 2.5 3  #2 3
+for qA in 5
 do
-for Ra in 2.0 #0.5 1.0 2.0 #1 1.5 2
+for EA in 5 #5 6 8 10 #6 7 8 #7 8 9 10 1
 do
-python3 make_simulation.py -nA ${nA} -nB ${nB} -eps_ll ${EPS} --eps_pp_attr ${EPSa} --protein_isotropic_attr_range ${Ra} --density_A_target 0.03 --real ${REAL} -qA ${qA} -qB ${qB} --mpinum ${mpinum} --runsteps 50000000 --ligand_range 0.2 --dumpstep 1000000
+for EP in 1.5 #0.5 1.0 1.5 2.0 2.5 3.0 #1 1.5 2.5 3  #2 3
+do
+for RA in 2.0 #0.5 1.0 2.0 #1 1.5 2
+do
+python3 make_simulation.py -nA ${nA} -ea ${EA} -ep ${EP} -ra ${RA} --density_A_target 0.03 --real ${REAL} -qA ${qA} -qB ${qB} --mpinum ${mpinum} --runsteps 50000000 --ligand_range 0.2 --dumpstep 1000000
 runscriptfile=$(ls -rt runscript_* | tail -n 1)
 echo $runscriptfile
 qsub $runscriptfile
