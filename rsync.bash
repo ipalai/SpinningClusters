711dc2430925052699092d8edb2790e7f98aea38
ipalai <>
2022-05-23 23:44:52 +0200

    analysis code: ecdf, cluster size vs time, fractal dimension...

--- rsync.bash
+++ rsync.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -l
 
-rsync -av --progress --include={'*.xyz','*.dat','*.txt','*.sh'} --include='*/' --exclude='*' ipalaia@marcin80.ista.local:/nfs/scistore15/saricgrp/ipalaia/SpinningClusters/Simulations/SpinningClusters/* ./
+rsync -av --progress --include={'TrajLog_*.xyz','Out_*.dat','Log_*.dat','RotationStatsMolecule*.dat','*.txt','*.sh'} --include='*/' --exclude='*' ipalaia@marcin80.ista.local:/nfs/scistore15/saricgrp/ipalaia/SpinningClusters/Simulations/SpinningClusters/* ./
 #rsync -av --progress -e "ssh -A -J ucapipa@socrates.ucl.ac.uk:22" ucapipa@myriad.rc.ucl.ac.uk:/lustre/home/ucapipa/Scratch/TheoryClustersNowca/* ./
 
