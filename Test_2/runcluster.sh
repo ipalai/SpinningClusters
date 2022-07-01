6146c03fa24572086f1d1b12db40d18a442e48fb
ipalai <40635629+ipalai@users.noreply.github.com>
2022-07-01 12:15:36 +0200

    README for Test_2

--- Test_2/runcluster.sh
+++ Test_2/runcluster.sh
@@ -2,13 +2,15 @@
 #module load python3/recommended
 
 nA=10000
-mpinum=9
-DENS=0.1
+mpinum=16
+DENS=0.05
 
 
-for REAL in 100 #101 102 103 104 105 106 107 #108 109 110 111 112 113 114 115
+for REAL in 200 201 202 #101 102 103 104 105 106 107 #108 109 110 111 112 113 114 115
 do
-for EXTTORQUE in 0 0.1 0.3 1 3 10 30 100
+for DENS in 0.20 # 0.05 0.10 0.15 0.25 0.40
+do
+for EXTTORQUE in 1  # 0 0.3 1 3 10 15
 do
 for qA in 5 #6 8
 do
@@ -19,12 +21,13 @@ do
 for RA in 0.15 #0.5 1.0 2.0 #1 1.5 2
 do
 RP=${RA}
-python3 make_simulation.py -nA ${nA} -ea ${EA} -ep ${EP} -ra ${RA} -rp ${RP} -dens ${DENS} --real ${REAL} -qA ${qA} -eT ${EXTTORQUE} --MPInum ${mpinum} --RunSteps 5000000 --dumpevery 50000 --MaxRunTime 200
+python3 make_simulation.py -nA ${nA} -ea ${EA} -ep ${EP} -ra ${RA} -rp ${RP} -dens ${DENS} --real ${REAL} -qA ${qA} -eT ${EXTTORQUE} --MPInum ${mpinum} --RunSteps 20000000 --dumpevery 500000 --MaxRunTime 240
 runscriptfile=$(ls -rt runscript_* | tail -n 1)
 echo $runscriptfile
 sbatch $runscriptfile
 mv $runscriptfile runscriptFiles/ 
-sleep 10
+sleep 5
+done
 done
 done
 done
