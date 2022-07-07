62e822703a5da71bc09caa3afff75aaeadaedc48
ipalai <>
2022-07-07 23:19:06 +0200

    runcluster
    Signed-off-by: ipalai <>

--- Test_8/runcluster.sh
+++ Test_8/runcluster.sh
@@ -2,15 +2,15 @@
 #module load python3/recommended
 
 mpinum=16
-qA=5
 EA=20
 EP=-10
 RA=0.15
 RP=${RA}
-DENS=0.40
-ETCLU=0.00
 
-for configfile in Input/Configurations/ConfigCluFrom_qA${qA}_dp0.50_dens${DENS}_eT${ETCLU}_nA10000_rp0.15_ra0.15_ep-10.0_ea20.0_T1.0_*/Config_C*.dat
+DENS=0.25
+
+
+for configfile in Input/Configurations/ConfigCluFrom_qA5_dp0.50_dens${DENS}_eT*_nA10000_rp0.15_ra0.15_ep-10.0_ea20.0_T1.0_202_*/Config_C*.dat
 #for configfile in Input/Configurations/ConfigCluFrom_*/Config_C*.dat
 do
 
@@ -25,15 +25,16 @@ do
 	do
 
 		filepattern=$(echo $configfile | sed 's/.*Configurations\/Config_//' | sed  's/\/Config.*//' | sed "s/_ts[0-9]*//")
-		if ls Results*/TrajLog_${filepattern}_${REAL}.xyz 1> /dev/null 2>&1
+		if ls Results*/Traj*_${filepattern}_${REAL}.xyz 1> /dev/null 2>&1
 		then
-			echo "TrajLog with same filepattern already exists! File was skipped."
+			echo "Traj with same filepattern already exists! File was skipped."
    			continue
 		fi
 
-		for EXTTORQUE in 0
+		EXTTORQUE=0
+		for DEFDIR in xy #x y xAy
 		do
-			python3 make_simulation_extractedcluster.py -ea ${EA} -ep ${EP} -ra ${RA} -rp ${RP} --real ${REAL} -eT ${EXTTORQUE} --MPInum ${mpinum} --MaxRunTime 240 --submitflag sbatch --configfile ${configfile}
+			python3 make_simulation_extractedcluster.py -ea ${EA} -ep ${EP} -ra ${RA} -rp ${RP} --real ${REAL} -eT ${EXTTORQUE} --MPInum ${mpinum} --MaxRunTime 240 --submitflag sbatch --configfile ${configfile} --DeformDirection ${DEFDIR}
 			sleep 5
 
 		done
