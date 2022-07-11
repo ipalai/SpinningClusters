ff563e4a29aaa0ced946aa475bab8eae5253ce87
ipalai <40635629+ipalai@users.noreply.github.com>
2022-07-11 19:54:22 +0200

    Test_2 readme and runclusters

--- Test_2/README.txt
+++ Test_2/README.txt
@@ -1,3 +1,5 @@
 Realisations 100, 101,... 115 are the core simulations used for most of the paper for density 0.20.
 
-Realisations 200, 201, ... are much longer simulations, run at various densities to build the phase diagram (Fig. 4 of the draft). 200 has no printed Restart files, 201 and 202 do and are therefore longer because they have been restarted.
+Realisations 200, 201, ... are much longer simulations, run at various densities to build the phase diagram (Fig. 4 of the draft). 200 has no printed Restart files, 201 and 202 do. The restarted simulations (ran through runcluster_restart.sh or restart_simulation.py) have file name patterns that look like
+ Traj_2_*, Traj_3_*, Traj_4_*, ...
+for, respectively, the 2nd restart, the 3rd, the 4th...
--- Test_2/runcluster_restart.sh
+++ Test_2/runcluster_restart.sh
@@ -7,9 +7,9 @@ mpinum=16
 
 for REAL in 201 202 #101 102 103 104 105 106 107 #108 109 110 111 112 113 114 115
 do
-for DENS in 0.40 # 0.05 0.10 0.15 0.25 0.40
+for DENS in 0.05 # 0.05 0.10 0.15 0.25 0.40
 do
-for EXTTORQUE in 0 0.3 1 3 # 0 0.1 0.3 1 3 5 10 15
+for EXTTORQUE in  0 0.3 1 3 10 15 # 0 0.1 0.3 1 3 5 10 15
 do
 for qA in 5 #6 8
 do
@@ -20,7 +20,7 @@ do
 for RA in 0.15 #0.5 1.0 2.0 #1 1.5 2
 do
 RP=${RA}
-python3 restart_simulation.py -nA ${nA} -ea ${EA} -ep ${EP} -ra ${RA} -rp ${RP} -dens ${DENS} --real ${REAL} -qA ${qA} -eT ${EXTTORQUE} --MPInum ${mpinum} --RunSteps 40000000 --dumpevery 500000 --MaxRunTime 240
+python3 restart_simulation.py -nA ${nA} -ea ${EA} -ep ${EP} -ra ${RA} -rp ${RP} -dens ${DENS} --real ${REAL} -qA ${qA} -eT ${EXTTORQUE} --MPInum ${mpinum} --RunSteps 100000000 --dumpevery 500000 --MaxRunTime 240
 runscriptfile=$(ls -rt runscript_* | tail -n 1)
 echo $runscriptfile
 sbatch $runscriptfile
